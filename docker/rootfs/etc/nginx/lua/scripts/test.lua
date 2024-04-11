99f11f61c513ad2c28651df32b74dd993cf5d3ab
root <897993946@qq.com>
2024-04-11 16:09:08 +0800

    test

--- docker/rootfs/etc/nginx/lua/scripts/test.lua
+++ docker/rootfs/etc/nginx/lua/scripts/test.lua
@@ -1,2 +1,17 @@
-local args = ngx.req.get_uri_args()
-ngx.say("Hello, ", args["name"] or "Lua!")
+local rip=waf.reqHeaders.x_forwarded_for
+if rip then
+	if type(rip) ~= "string" then
+		return true,"Malform X-Forwarded-For",true
+	elseif waf.contains(rip,"'") then
+		return true,rip,true
+	end
+end
+rip=waf.reqHeaders.client_ip
+if rip then
+	if type(rip) ~= "string" then
+		return true,"Malform Client-IP",true
+	elseif waf.contains(rip,"'") then
+		return true,rip,true
+	end
+end
+return false
