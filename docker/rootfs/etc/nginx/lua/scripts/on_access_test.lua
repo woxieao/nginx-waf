d4f36950d41da1a06da26c256716b4adc2e012d2
xiea <867993946@qq.com>
2024-04-18 11:59:47 +0800

    提交test

--- docker/rootfs/etc/nginx/lua/scripts/on_access_test.lua
+++ docker/rootfs/etc/nginx/lua/scripts/on_access_test.lua
@@ -1,6 +1,6 @@
--- local test_arg = ngx.var.arg_test_arg
--- if test_arg == "get" then
---     ngx.header["test-header"] = "get" 
--- else
---     ngx.header["test-header"] = "not get" .. test_arg
--- end
+local test_arg = ngx.var.arg_test_arg
+if test_arg == "get" then
+    ngx.header["test-header"] = "get"
+else
+    ngx.header["test-header"] = "not get" .. tostring(test_arg)
+end
