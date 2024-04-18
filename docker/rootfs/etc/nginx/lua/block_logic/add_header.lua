fdcd5f3094d714d0212fe4d70030248151684d73
xiea <867993946@qq.com>
2024-04-18 16:54:42 +0800

    提交test

---
+++ docker/rootfs/etc/nginx/lua/block_logic/add_header.lua
@@ -0,0 +1,2 @@
+local test_arg = ngx.var.arg_test_arg
+ngx.header["test_arg "] = "test_arg " .. tostring(test_arg)
--- docker/rootfs/etc/nginx/lua/scripts/on_access_test.lua
+++ docker/rootfs/etc/nginx/lua/scripts/on_access_test.lua
@@ -1,6 +1,22 @@
-local test_arg = ngx.var.arg_test_arg
-if test_arg == "get" then
-    ngx.header["test-header"] = "get"
-else
-    ngx.header["test-header"] = "not get" .. tostring(test_arg)
+local lfs = require("lfs")
+
+-- 指定要读取的目录路径
+local directory_path = "/etc/nginx/lua/block_logic"
+
+-- 遍历目录中的文件
+for file in lfs.dir(directory_path) do
+    -- 拼接文件的完整路径
+    local full_path = directory_path .. "/" .. file
+
+    -- 检查文件是否是 Lua 脚本
+    if file:match("%.lua$") and lfs.attributes(full_path, "mode") == "file" then
+        -- 加载并执行 Lua 脚本
+        local chunk, err = loadfile(full_path)
+        if chunk then
+            print("Executing script:", file)
+            chunk()
+        else
+            print("Error loading script:", err)
+        end
+    end
 end
