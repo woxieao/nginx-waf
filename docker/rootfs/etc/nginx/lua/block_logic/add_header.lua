382c95e536d076fa68978e56223e257ffc128bdc
xiea <867993946@qq.com>
2024-04-18 17:08:17 +0800

    提交test

--- docker/rootfs/etc/nginx/lua/block_logic/add_header.lua
+++
@@ -1,2 +0,0 @@
-local test_arg = ngx.var.arg_test_arg
-ngx.header["test_arg"] = "test_arg " .. tostring(test_arg)
--- docker/rootfs/etc/nginx/lua/scripts/on_access_test.lua
+++ docker/rootfs/etc/nginx/lua/scripts/on_access_test.lua
@@ -1,22 +1,21 @@
-local lfs = require("lfs")
+local cjson = require("cjson")
 
--- 指定要读取的目录路径
-local directory_path = "/etc/nginx/lua/block_logic"
+-- 指定要读取的 JSON 文件路径
+local json_file_path = "/etc/nginx/lua/configs/lua_list.json"
 
--- 遍历目录中的文件
-for file in lfs.dir(directory_path) do
-    -- 拼接文件的完整路径
-    local full_path = directory_path .. "/" .. file
+-- 读取 JSON 文件内容
+local file = io.open(json_file_path, "r")
+if file then
+    local json_data = file:read("*a")
+    file:close()
 
-    -- 检查文件是否是 Lua 脚本
-    if file:match("%.lua$") and lfs.attributes(full_path, "mode") == "file" then
-        -- 加载并执行 Lua 脚本
-        local chunk, err = loadfile(full_path)
-        if chunk then
-            print("Executing script:", file)
-            chunk()
-        else
-            print("Error loading script:", err)
-        end
+    -- 反序列化 JSON 数据
+    local decoded_data = cjson.decode(json_data)
+
+    for i, value in ipairs(decoded_data) do
+        ngx.say(cjson.encode(value) .. "\n")
     end
+
+else
+    print("Error opening JSON file")
 end
