94a18eae022d28a8ae8be88311dce64748c61919
xiea <867993946@qq.com>
2024-04-18 17:15:00 +0800

    提交test

--- docker/rootfs/etc/nginx/lua/scripts/on_access_test.lua
+++ docker/rootfs/etc/nginx/lua/scripts/on_access_test.lua
@@ -1,21 +1,19 @@
 local cjson = require("cjson")
 
--- 指定要读取的 JSON 文件路径
 local json_file_path = "/etc/nginx/lua/configs/lua_list.json"
 
--- 读取 JSON 文件内容
 local file = io.open(json_file_path, "r")
 if file then
     local json_data = file:read("*a")
     file:close()
-
-    -- 反序列化 JSON 数据
     local decoded_data = cjson.decode(json_data)
 
-    for i, value in ipairs(decoded_data) do
-        ngx.say(cjson.encode(value) .. "\n")
+    for i, luaInfo in ipairs(decoded_data) do
+
+        if luaInfo.isEnable then
+            local lua_func, err = loadstring(luaInfo.script)
+            lua_func();
+        end
     end
 
-else
-    print("Error opening JSON file")
 end
