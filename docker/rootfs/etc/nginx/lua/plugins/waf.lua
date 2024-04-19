436c0f8f0a78c02c2edbd7d666444445e93e63d2
xiea <867993946@qq.com>
2024-04-19 15:18:43 +0800

    提交test

---
+++ docker/rootfs/etc/nginx/lua/plugins/waf.lua
@@ -0,0 +1,23 @@
+local function mainBody()
+
+    local cjson = require("cjson")
+
+    local json_file_path = "/etc/nginx/lua/configs/lua_list.json"
+
+    local file = io.open(json_file_path, "r")
+    if file then
+        local json_data = file:read("*a")
+        file:close()
+        local decoded_data = cjson.decode(json_data)
+
+        for i, luaInfo in ipairs(decoded_data) do
+
+            if luaInfo.isEnable then
+                local lua_func, err = loadstring(luaInfo.script)
+                lua_func();
+            end
+        end
+
+    end
+end
+return mainBody;
