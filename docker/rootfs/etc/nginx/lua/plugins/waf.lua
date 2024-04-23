4368842e21baecb6473ec28bfecfa0b4b25cde99
xiea <867993946@qq.com>
2024-04-23 18:48:31 +0800

    提交test

--- docker/rootfs/etc/nginx/lua/plugins/waf.lua
+++ docker/rootfs/etc/nginx/lua/plugins/waf.lua
@@ -1,17 +1,24 @@
 local function mainBody()
-
     function listLuaFiles(folder_path)
-        local files = {}
-        local dir = io.popen("ls -v " .. folder_path)
-        for file in dir:lines() do
-            if file:match("%.lua$") then 
-                table.insert(files, file) 
+        local shared_data = ngx.shared.shared_data
+        local waf_detectors_key_name = "waf_detectors";
+        if shared_data == nil then
+            local cjson = require "cjson";
+            local files = {}
+            local dir = io.popen("ls -v " .. folder_path)
+            for file in dir:lines() do
+                if file:match("%.lua$") then
+                    table.insert(files, file)
+                end
             end
+            dir:close()
+            shared_data:set(waf_detectors_key_name, cjson.encode(files))
+            return files;
+        else
+            return cjson.decode(shared_data:get(waf_detectors_key_name));
         end
-        dir:close()
-        return files
     end
-
+    -- 加载的模块会被缓存
     local files = listLuaFiles("/etc/nginx/lua/waf_detectors/")
 
     for _, moduleName in ipairs(files) do
