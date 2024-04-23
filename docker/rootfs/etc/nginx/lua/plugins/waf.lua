5e2e2866053689b78324f60224e69700ac9a9046
xiea <867993946@qq.com>
2024-04-23 18:30:29 +0800

    提交test

--- docker/rootfs/etc/nginx/lua/plugins/waf.lua
+++ docker/rootfs/etc/nginx/lua/plugins/waf.lua
@@ -2,10 +2,10 @@ local function mainBody()
 
     function listLuaFiles(folder_path)
         local files = {}
-        local dir = io.popen("ls " .. folder_path)  -- 使用ls命令列出文件夹中的所有文件
+        local dir = io.popen("ls -v " .. folder_path)
         for file in dir:lines() do
-            if file:match("%.lua$") then  -- 判断文件是否以.lua结尾
-                table.insert(files, file)  -- 将符合条件的文件名添加到数组中
+            if file:match("%.lua$") then 
+                table.insert(files, file) 
             end
         end
         dir:close()
@@ -14,9 +14,9 @@ local function mainBody()
 
     local files = listLuaFiles("/etc/nginx/lua/waf_detectors/")
 
-    local str = "";
-    local array = {10, 20, 30}
-    for _, value in ipairs(files) do str = str .. value .. "|"; end
-    ngx.say(str);
+    for _, moduleName in ipairs(files) do
+        local module = require(moduleName)
+        module()
+    end
 end
 return mainBody;
