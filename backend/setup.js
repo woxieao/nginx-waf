0a14371e03311eefa4598de1d6663697807f47fb
xiea <867993946@qq.com>
2024-04-28 18:25:28 +0800

    提交test

--- backend/setup.js
+++ backend/setup.js
@@ -209,8 +209,9 @@ const setupWafScripts = () => {
 								"111.111.111.111", "222.222.222.222"
 								-- 添加其他需要加入黑名单的IP地址
 							}
+							local client_ip = ngx.var.remote_addr
 							for _, black_ip in ipairs(blacklist_ips) do
-								if black_ip == ip then return true end
+								if black_ip == client_ip then return true end
 							end
 							return false
 							`,
--- docker/rootfs/etc/nginx/lua/modules/cache_mgr.lua
+++ docker/rootfs/etc/nginx/lua/modules/cache_mgr.lua
@@ -1,9 +1,3 @@
--- local moudle = {}
-
--- function moudle.get()
-
-  
-
--- end
-
--- return moudle
+-- /etc/nginx/bin/resty -e '
+if ngx.shared.shared_data ~= nil then    print(ngx.shared.shared_data:get("waf_detectors_list")) end
+-- '
