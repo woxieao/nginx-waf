a263165cc044086bf119cd49f630ed3376f5207e
xiea <867993946@qq.com>
2024-05-06 16:20:04 +0800

    提交test

--- docker/rootfs/etc/nginx/lua/api_services/get_exec_counter.lua
+++ docker/rootfs/etc/nginx/lua/api_services/get_exec_counter.lua
@@ -2,11 +2,11 @@ if ngx.shared.exec_counter ~= nil then
     local rule_id = ngx.var.arg_rule_id;
     local count = ngx.shared.exec_counter:get('r_' .. rule_id)
     if count == nil then
-        ngx.say(0)
+        ngx.say("a")
     else
         ngx.say(count)
     end
 
 else
-    ngx.say(0)
+    ngx.say("b")
 end
