531e9c4e6a0fc40c9c39e5a9911bf700ccc6faf8
xiea <867993946@qq.com>
2024-05-06 17:16:18 +0800

    提交test

--- docker/rootfs/etc/nginx/lua/api_services/get_counter.lua
+++ docker/rootfs/etc/nginx/lua/api_services/get_counter.lua
@@ -4,11 +4,12 @@ local key_name = 'r_' .. rule_id;
 local function get_exec_counter()
     if ngx.shared.exec_counter ~= nil then
         local count = ngx.shared.exec_counter:get(key_name);
-        -- Get the latest counter and reset it to zero
-        ngx.shared.exec_counter:incr(key_name, -count);
+
         if count == nil then
             return 0;
         else
+            -- Get the latest counter and reset it to zero
+            ngx.shared.exec_counter:incr(key_name, -count);
             return count;
         end
     else
@@ -19,11 +20,12 @@ end
 local function get_block_counter()
     if ngx.shared.block_counter ~= nil then
         local count = ngx.shared.block_counter:get(key_name);
-        -- Get the latest counter and reset it to zero
-        ngx.shared.block_counter:incr(key_name, -count);
+
         if count == nil then
             return 0;
         else
+            -- Get the latest counter and reset it to zero
+            ngx.shared.block_counter:incr(key_name, -count);
             return count;
         end
     else
