8b43687c28c3363d7f6dbd9b25c53ee1affef9e2
xiea <867993946@qq.com>
2024-05-06 16:34:55 +0800

    提交test

--- docker/rootfs/etc/nginx/lua/api_services/get_block_counter.lua
+++ docker/rootfs/etc/nginx/lua/api_services/get_block_counter.lua
@@ -1,6 +1,12 @@
 if ngx.shared.block_counter ~= nil then
     local rule_id = ngx.var.arg_rule_id;
-    ngx.say(ngx.shared.block_counter:get('r_' .. rule_id))
+    local count = ngx.shared.block_counter:get('r_' .. rule_id)
+    if count == nil then
+        ngx.say(0)
+    else
+        ngx.say(count)
+    end
+
 else
-    ngx.say('0')
+    ngx.say(0)
 end
