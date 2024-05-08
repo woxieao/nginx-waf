a3ed7e3f68467acedb1396df64d91d2d7613cb93
xiea <867993946@qq.com>
2024-05-08 14:50:44 +0800

    提交test

--- docker/rootfs/etc/nginx/lua/modules/qps_log.lua
+++ docker/rootfs/etc/nginx/lua/modules/qps_log.lua
@@ -1,4 +1,5 @@
-local qps_key_prefix = "0_";
+local qps_second_key_prefix = "0_";
+local qps_second_key_prefix = "0_";
 
 local dict_counter = require "dict_counter";
 local cjson = require "cjson";
@@ -6,14 +7,21 @@ local qps_log = {}
 local dict = ngx.shared.qps_log_data;
 
 function qps_log.log_request()
-    dict_counter.incr_counter(dict, qps_key_prefix .. os.time())
+    local timestamp = os.time();
+    local secondTimestamp = os.date("%Y%m%d%H%M%S", timestamp);
+    dict_counter.incr_counter(dict, qps_second_key_prefix .. secondTimestamp)
 end
 function qps_log.log2json(start_time, end_time)
-    local result = {};
+
+    local timestamp = os.time();
+    local secondTimestamp = os.date("%Y%m%d%H%M%S", timestamp);
+    ngx.say(secondTimestamp);
+
+    local result = {qps = {}, qpm = {}, qph = {}, qpd = {}};
     for i = start_time, end_time do
-        result[tostring(i)] = (dict:get(qps_key_prefix .. i) or 0);
-        --    ngx.say(qps_key_prefix .. i, dict:get(qps_key_prefix .. i) or 0)
+        result.qps[tostring(i)] = (dict:get(qps_second_key_prefix .. i) or 0);
     end
     return cjson.encode(result);
 end
 return qps_log;
+
