08fca7876bcbaed8d870c8fb99d25510e5aedb91
xiea <867993946@qq.com>
2024-05-08 15:13:15 +0800

    提交test

--- docker/rootfs/etc/nginx/lua/api_services/debug.lua
+++ docker/rootfs/etc/nginx/lua/api_services/debug.lua
@@ -3,4 +3,4 @@ local qps_log = require "qps_log";
 
 local helpers = require "helpers"
 
-ngx.say(qps_log.log2json(os.time() - 60, os.time()))
+ngx.say(qps_log.log2json(tostring(os.time() - 60), os.time()))
--- docker/rootfs/etc/nginx/lua/modules/qps_log.lua
+++ docker/rootfs/etc/nginx/lua/modules/qps_log.lua
@@ -1,7 +1,7 @@
-local qps_second_key_prefix = "0_";
-local qps_minute_key_prefix = "1_";
-local qps_hour_key_prefix = "2_";
-local qps_day_key_prefix = "3_";
+local qps_key_prefix = "0_";
+local qpm_key_prefix = "1_";
+local qph_key_prefix = "2_";
+local qpd_key_prefix = "3_";
 
 local dict_counter = require "dict_counter";
 local helpers = require "helpers";
@@ -16,29 +16,29 @@ function qps_log.log_request()
     local hourTimestamp = string.sub(secondTimestamp, 1, -5);
     local dayTimestamp = string.sub(secondTimestamp, 1, -7);
 
-    dict_counter.incr_counter(dict, qps_second_key_prefix .. secondTimestamp)
-    dict_counter.incr_counter(dict, qps_minute_key_prefix .. minuteTimestamp)
-    dict_counter.incr_counter(dict, qps_hour_key_prefix .. hourTimestamp)
-    dict_counter.incr_counter(dict, qps_day_key_prefix .. dayTimestamp)
+    dict_counter.incr_counter(dict, qps_key_prefix .. secondTimestamp)
+    dict_counter.incr_counter(dict, qpm_key_prefix .. minuteTimestamp)
+    dict_counter.incr_counter(dict, qph_key_prefix .. hourTimestamp)
+    dict_counter.incr_counter(dict, qpd_key_prefix .. dayTimestamp)
 end
 function qps_log.log2json(start_time, end_time)
+    local str_len = tostring(start_time);
+    start_time = tonumber(start_time)
+    end_time = tonumber(end_time)
+    local key_prefix;
+    if str_len == 14 then
+        key_prefix = qps_key_prefix;
+    elseif str_len == 12 then
+        key_prefix = qpm_key_prefix;
+    elseif str_len == 10 then
+        key_prefix = qph_key_prefix;
+    elseif str_len == 8 then
+        key_prefix = qpd_key_prefix;
+    end
 
-    local secondTimestamp = helpers.get_current_time_str();
-    local minuteTimestamp = string.sub(secondTimestamp, 1, -3);
-    local hourTimestamp = string.sub(secondTimestamp, 1, -5);
-    local dayTimestamp = string.sub(secondTimestamp, 1, -7);
-
-    ngx.say(secondTimestamp);
-
-    ngx.say(minuteTimestamp);
-
-    ngx.say(hourTimestamp);
-
-    ngx.say(dayTimestamp);
-
-    local result = {qps = {}, qpm = {}, qph = {}, qpd = {}};
+    local result = {};
     for i = start_time, end_time do
-        result.qps[tostring(i)] = (dict:get(qps_second_key_prefix .. i) or 0);
+        result[tostring(i)] = (dict:get(key_prefix .. i) or 0);
     end
     return cjson.encode(result);
 end
