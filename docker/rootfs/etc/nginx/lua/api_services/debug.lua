ae1bc1c78a7082ae346f629fcce10533c5fcec50
xiea <867993946@qq.com>
2024-05-08 15:14:55 +0800

    提交test

--- docker/rootfs/etc/nginx/lua/api_services/debug.lua
+++ docker/rootfs/etc/nginx/lua/api_services/debug.lua
@@ -3,4 +3,5 @@ local qps_log = require "qps_log";
 
 local helpers = require "helpers"
 
-ngx.say(qps_log.log2json(tostring(os.time() - 60), os.time()))
+ngx.say(qps_log.log2json(helpers.get_current_time_str(),
+                         tonumber(helpers.get_current_time_str())) + 60)
