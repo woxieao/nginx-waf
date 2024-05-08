a10ae8679a099e65ca0c778689d9cbc5c042108b
xiea <867993946@qq.com>
2024-05-08 18:48:25 +0800

    提交test

--- docker/rootfs/etc/nginx/lua/api_services/debug.lua
+++ docker/rootfs/etc/nginx/lua/api_services/debug.lua
@@ -2,6 +2,7 @@ local counter_log = require "counter_log";
 local qps_log = require "qps_log";
 
 local helpers = require "helpers"
+ngx.say(ngx.http_user_agent)
 
 ngx.say(counter_log.log2json(helpers.get_current_time(),
                          helpers.get_current_time() + 60 * 60 * 10, "h"))
