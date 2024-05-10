daea9e7dfd99b48d228150e12a76142c0df2c886
xiea <867993946@qq.com>
2024-05-10 16:30:16 +0800

    提交test

--- frontend/js/app/dashboard/charts/qps/main.js
+++ frontend/js/app/dashboard/charts/qps/main.js
@@ -35,5 +35,27 @@ module.exports = Mn.View.extend({
       ],
     });
   },
-  onRender: function () {},
+  onRender: function () {
+    console.log(77777, document.getElementsByClassName("echart-sm")[0]);
+    var myChart = echartsTest.init(
+      document.getElementsByClassName("echart-sm")[0]
+    );
+    myChart.setOption({
+      title: {
+        text: "ECharts 入门示例",
+      },
+      tooltip: {},
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "销量",
+          type: "bar",
+          data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+        },
+      ],
+    });
+  },
 });
