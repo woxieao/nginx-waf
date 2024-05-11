70277d508dfbc35266488d70d999c89d1ca55dc5
xiea <867993946@qq.com>
2024-05-11 17:34:46 +0800

    提交test

--- docker/rootfs/etc/nginx/lua/api_services/debug.lua
+++ docker/rootfs/etc/nginx/lua/api_services/debug.lua
@@ -4,4 +4,15 @@ local qps_log = require "qps_log";
 local helpers = require "helpers"
 ngx.header.content_type = "application/json; charset=utf-8"
 ngx.say("222")
-ngx.say(ngx.var.http_user_agent)
+
+local function get_os_from_ua(userAgent)
+    if userAgent ~= nil then
+        if string.find(userAgent, "Win") then return "Windows"; end
+        if string.find(userAgent, "Mac") then return "macOS"; end
+        if string.find(userAgent, "Linux") then return "Linux"; end
+        if string.find(userAgent, "Android") then return "Android"; end
+        if string.find(userAgent, "like Mac") then return "iOS"; end
+    end
+    return "Others";
+end
+ngx.say(get_os_from_ua(ngx.var.http_user_agent))
