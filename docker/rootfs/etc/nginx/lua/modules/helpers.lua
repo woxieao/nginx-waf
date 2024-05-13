2b94d331b112e232df9159c89b742d371dbe99ac
xiea <867993946@qq.com>
2024-05-13 18:04:45 +0800

    提交test

--- docker/rootfs/etc/nginx/lua/modules/helpers.lua
+++ docker/rootfs/etc/nginx/lua/modules/helpers.lua
@@ -12,16 +12,9 @@ function helpers.get_current_time_str(time)
 end
 
 function helpers.arr_contains(request_arr, matches_arr)
-    ngx.say(1)
     for id, request_arg in pairs(request_arr) do
-        ngx.say(2)
-        ngx.say(id)
-        ngx.say(3)
-        ngx.say(request_arg)
-        ngx.say(4)
         if request_arg ~= nil then
             request_arg = string.lower(request_arg)
-
             for _, match in ipairs(matches_arr) do
                 if string.find(request_arg, match, 1, true) ~= nil then
                     return true
@@ -29,7 +22,6 @@ function helpers.arr_contains(request_arr, matches_arr)
             end
         end
     end
-    ngx.say(5)
     return false
 end
 
