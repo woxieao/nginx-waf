298067b9dd8db965def370415a7afc0badc8524b
xiea <867993946@qq.com>
2024-05-14 17:33:06 +0800

    提交test

--- docker/rootfs/etc/nginx/lua/api_services/debug.lua
+++ docker/rootfs/etc/nginx/lua/api_services/debug.lua
@@ -1,11 +1,12 @@
+local cjson = require "cjson";
 local pattern = "<[\\s]*(iframe|script|body|img|layer|div|meta|style|base|object|input)";
 local str = ngx.req.get_uri_args()['test'];
+
 local captures, err = ngx.re.match(str, pattern, "jo")
 
 ngx.say("start")
 if captures then
-    ngx.say(captures)
-    ngx.say(err)
+    ngx.say(cjson.encode(captures))
 else
     ngx.say("not matched")
 end
