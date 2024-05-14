f3654a9fd84870f199b330c0ce8f35a0559e8725
xiea <867993946@qq.com>
2024-05-14 17:30:23 +0800

    提交test

--- docker/rootfs/etc/nginx/lua/api_services/debug.lua
+++ docker/rootfs/etc/nginx/lua/api_services/debug.lua
@@ -1,10 +1,11 @@
 local pattern = "<[\\s]*(iframe|script|body|img|layer|div|meta|style|base|object|input)";
 local str = ngx.req.get_uri_args()['test'];
-local arg = ngx.re.match(str, pattern, 'jo');
+local captures, err = ngx.re.match(str, pattern, "jo")
 
 ngx.say("start")
 if arg then
-    ngx.say(arg)
+    ngx.say(captures)
+    ngx.say(err)
 else
     ngx.say("not matched")
 end
