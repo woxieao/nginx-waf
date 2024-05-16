0f26724b72776feb11ca97ad096dde2003201214
xiea <867993946@qq.com>
2024-05-16 16:06:34 +0800

    提交test

--- docker/rootfs/etc/nginx/lua/api_services/debug.lua
+++ docker/rootfs/etc/nginx/lua/api_services/debug.lua
@@ -1,15 +1,2 @@
-local cjson = require "cjson";
-local str_list = {};
-
-local args = ngx.req.get_uri_args();
-for _, value in pairs(args) do
-    table.insert(str_list, value);
-end
-local headers = ngx.req.get_headers();
-for _, value in pairs(headers) do
-    table.insert(str_list, value);
-end
-ngx.req.read_body()
-table.insert(str_list, ngx.req.get_body_data());
-
-ngx.say(cjson.encode(str_list));
+ngx.say("你的请求已被拦截")
+ngx.exit(ngx.ctx.status_code or ngx.HTTP_FORBIDDEN)
