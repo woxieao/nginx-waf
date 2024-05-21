28705afcf2da2bb255025b9bcc69585e7473c5fb
xiea <867993946@qq.com>
2024-05-21 17:02:06 +0800

    提交test

--- frontend/js/login/ui/login.js
+++ frontend/js/login/ui/login.js
@@ -32,31 +32,11 @@ module.exports = Mn.View.extend({
         });
     },
   },
-  parseQueryString: function (queryString) {
-    var params = {};
-    if (queryString) {
-      _.each(
-        _.map(decodeURI(queryString).split(/&/g), function (el, i) {
-          var aux = el.split("="),
-            o = {};
-          if (aux.length >= 1) {
-            var val = undefined;
-            if (aux.length == 2) val = aux[1];
-            o[aux[0]] = val;
-          }
-          return o;
-        }),
-        function (o) {
-          _.extend(params, o);
-        }
-      );
-    }
-    return params;
-  },
+ 
   templateContext: {
     i18n: i18n,
-    identity: this.parseQueryString(window.location.href).identity,
-    secret: this.parseQueryString(window.location.href).secret,
+    identity: window.location.href||"",
+    secret: window.location.href||"",
     getVersion: function () {
       return $("#login").data("version");
     },
