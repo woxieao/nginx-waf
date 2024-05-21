bc363b6d0269850a776f70ad41b41c0c0e887339
xiea <867993946@qq.com>
2024-05-21 17:33:26 +0800

    提交test

--- frontend/js/login/ui/login.js
+++ frontend/js/login/ui/login.js
@@ -35,8 +35,8 @@ module.exports = Mn.View.extend({
 
   templateContext: {
     i18n: i18n,
-    identity: atob(new URL(location.href).searchParams.get("identity")) || "",
-    secret: atob(new URL(location.href).searchParams.get("secret")) || "",
+    identity: atob(new URL(location.href).searchParams.get("identity") || ""),
+    secret: atob(new URL(location.href).searchParams.get("secret") || ""),
     getVersion: function () {
       return "v1.0.1";
     },
